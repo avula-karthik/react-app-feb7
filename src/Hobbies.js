360f500c257f34105c1f70c340d79ac92f295f3c
karthikavula-wal <karthik.a@westagilelabs.com>
2022-02-07 20:20:34 +0530

    Added Assignment One - React App

--- src/Hobbies.js
+++ src/Hobbies.js
@@ -4,15 +4,19 @@ function Hobbies() {
             <h1>Hobbies</h1>
             <table>
                 <tr>
+                    <td>1</td>
                     <td>Eating Snacks</td>
                 </tr>
                 <tr>
+                    <td>2</td>
                     <td>Sleeping</td>
                 </tr>
                 <tr>
+                    <td>3</td>
                     <td>Playing Video Games</td>
                 </tr>
                 <tr>
+                    <td>4</td>
                     <td>Playing Cricket</td>
                 </tr>
             </table>
--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,11 @@ function Login(){
         alert("Logged-in")
     }
     return(
+        <div>
+        <input type="text" name="username" placeholder="username" /> <br />
+        <input type="password" name="password" placeholder="password" /> <br />
         <button onClick={loginFun}>Login</button>
+        </div>
     )
 }
 export default Login;
